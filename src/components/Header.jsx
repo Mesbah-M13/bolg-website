71cf51fec38387dbdeb0493d88668245692f0b5b
Mesbah-Himel <mesbah0010@gmail.com>
2022-09-05 16:11:18 +0600

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from '../assets/lws.svg'
+import logo from '../assets/travel.svg'
 const Header = () => {
   return (
     <>
       <nav className="bg-slate-100 shadow-md">
         <div className="max-w-7xl mx-auto px-5 lg:px-0 flex justify-between py-3 items-center">
           <a href="index.html">
-            <img className="h-10" src={logo} alt="Learn with Sumit" />
+            <img className="h-10" src={logo} alt="Travel and Explore" />
           </a>
         </div>
       </nav>
